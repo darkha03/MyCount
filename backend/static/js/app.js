86145800a5e18691fc31a07c00a388db27838893
darkhan03 <steve.khanhnguyen@gmail.com>
2026-02-15 12:40:14 +0100

    update suggest changes

--- backend/static/js/app.js
+++ backend/static/js/app.js
@@ -402,7 +402,7 @@ function renderStatisticChart() {
       plugins: {
         title: {
           display: true,
-          text: `Plan total expense: $${planTotal.toFixed(2)}`,
+          text: `Plan total expense: ${planTotal.toFixed(2)}`,
           font: { size: 16 }
         },
         legend: { position: 'top' }
